c18a3a1852ffacc0a140b3abb41c57704196405d
ccoli <hope1053@naver.com>
2022-10-24 22:31:45 +0900

    feat: 메인 컬러 Asset 추가 및 Extension 구현

---
+++ book-store-ccoli/book-store-ccoli/Extension/UIColor+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extension.swift
+//  book-store-ccoli
+//
+//  Created by 최혜린 on 2022/10/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ccoliGreen: UIColor {
+        return UIColor(named: "ccoliGreen") ?? .green
+    }
+}
