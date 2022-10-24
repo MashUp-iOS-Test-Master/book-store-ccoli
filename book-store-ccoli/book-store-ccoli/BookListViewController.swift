db8f2d3540ec3e9211092a87da10a253f821a527
ccoli <hope1053@naver.com>
2022-10-24 21:56:35 +0900

    feat: 스토리보드 삭제 및 code based 적용

---
+++ book-store-ccoli/book-store-ccoli/BookListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BookListViewController.swift
+//  book-store-ccoli
+//
+//  Created by 최혜린 on 2022/10/24.
+//
+
+import UIKit
+
+class BookListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
