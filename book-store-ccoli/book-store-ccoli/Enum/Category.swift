aa31f1fc3d61ebac3fe51dfa3758f0d13ee632c3
ccoli <hope1053@naver.com>
2022-10-25 22:30:06 +0900

    feat: enum Category 생성

---
+++ book-store-ccoli/book-store-ccoli/Enum/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  book-store-ccoli
+//
+//  Created by 최혜린 on 2022/10/25.
+//
+
+import Foundation
+
+enum Category: Int {
+    case novel
+    case tech
+    case economy
+    case poet
+}
